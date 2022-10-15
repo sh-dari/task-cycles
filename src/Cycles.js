28df39fc1063bdd5c8b92492bc391c12c3d599bd
Дарья Шипилова <sh.dari@list.ru>
2022-10-16 00:04:36 +0300

    task-cycles

--- src/Cycles.js
+++ src/Cycles.js
@@ -3,18 +3,47 @@
   Используя цикл for, просуммируйте все четные числа в диапазоне между этими значениями (включительно)
   и верните итоговый результат.
 */
-export function rangeSum(start, end) {}
+export function rangeSum(start, end) {
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        if (i % 2 == 1) {
+            continue;
+        }
+        sum += i;
+    }
+    return sum;
+}
 
 /*
   В функцию iterationCount() приходит неотрицательное число.
   Используя цикл while, выполняйте деление этого числа на два до тех пор, пока результат деления больше 0.1
   и верните количество потребовавшихся итераций (т.е. сколько раз пришлось выполнить деление).
 */
-export function iterationCount(a) {}
+export function iterationCount(a) {
+    let count = 0;
+    while (a > 0.1) {
+        a /= 2;
+        count++;
+    }
+    return count;
+}
 
 /*
   В функцию symbolsReplace() приходит строка текста.
   Используя цикл do while, замените в тексте каждый третий символ на символ нижнего подчеркивания
   и верните итоговый результат.
 */
-export function symbolsReplace(message) {}
+export function symbolsReplace(message) {
+    let i = -1;
+    let len = message.length - 3;
+    do {
+        if (len < 0) {
+            break;
+        }
+        i = i + 3;
+        let chars = message.split('');
+        chars[i] = '_';
+        message = chars.join('');
+    } while (i < len);
+    return message;
+}
